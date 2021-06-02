d2884fe0a28ff55f066a89933dc3fa18387f856c
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-03 00:21:59 +0900

    add : add JwtTokenProvider class for creating Jwt token

---
+++ src/main/java/com/codesquad/airbnb/JwtTokenProvider.java
@@ -0,0 +1,59 @@
+package com.codesquad.airbnb;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
+
+    private String secretKey;
+    private long validityInMilliseconds;
+
+    public JwtTokenProvider(@Value("&{security.jwt.token.secret-key}") String secretKey, @Value("${security.jwt.token.expire-length}") long validityInMilliseconds) {
+        this.secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        this.validityInMilliseconds = validityInMilliseconds;
+    }
+
+    // 토큰 생성
+    public String createToken(String subject) {
+        Claims claims = Jwts.claims().setSubject(subject);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+        logger.info("now: {}", now);
+        logger.info("validity: {}", validity);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    // 토큰에서 값 추출
+    public String getSubject(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    // 유효한 토큰인지 확인
+    public boolean validateToken(String token) {
+        Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+        return !claims.getBody().getExpiration().before(new Date());
+    }
+}
