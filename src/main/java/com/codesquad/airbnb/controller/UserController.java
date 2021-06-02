8f92a9739ea4e57449a3c12a07058a1e799f4a26
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-03 03:26:28 +0900

    add : Request의 Token을 통해 User 반환하는 기능 추가

--- src/main/java/com/codesquad/airbnb/controller/UserController.java
+++ src/main/java/com/codesquad/airbnb/controller/UserController.java
@@ -1,16 +1,18 @@
 package com.codesquad.airbnb.controller;
 
+import com.codesquad.TokenResponse;
+import com.codesquad.airbnb.domain.User;
 import com.codesquad.airbnb.oauth.GoogleUser;
 import com.codesquad.airbnb.oauth.OAuthToken;
-import com.codesquad.airbnb.domain.User;
 import com.codesquad.airbnb.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -25,7 +27,7 @@ public class UserController {
     }
 
     @GetMapping("/oauth/google/callback")
-    public ResponseEntity oauthLogin(String code) {
+    public ResponseEntity<TokenResponse> oauthLogin(String code) {
         ResponseEntity<String> accessTokenResponse = userService.createPost(code);
         OAuthToken oAuthToken = userService.getAccessToken(accessTokenResponse);
         logger.info("Access Token: {}", oAuthToken.getAccessToken());
@@ -33,12 +35,21 @@ public class UserController {
         ResponseEntity<String> userInfoResponse = userService.createGet(oAuthToken);
         GoogleUser googleUser = userService.getUserInfo(userInfoResponse);
         logger.info("Google User Name: {}", googleUser.getName());
-        
+
+        User user = googleUser.toUser(oAuthToken.getAccessToken());
+
         if (!userService.isJoinedUser(googleUser)) {
-            User user = googleUser.toUser(oAuthToken.getAccessToken());
             userService.save(user);
         }
 
-        return new ResponseEntity("로그인 성공", HttpStatus.OK);
+        String token = userService.createToken(user);
+        return ResponseEntity.ok().body(new TokenResponse(token, "bearer"));
+    }
+
+    @GetMapping("/info")
+    public ResponseEntity<User> getUserFromToken(HttpServletRequest request) {
+        String name = (String) request.getAttribute("name");
+        User user = userService.findByName(name);
+        return ResponseEntity.ok().body(user);
     }
 }
