f6fea525be449023a7d8881c8540dcaa806aaeab
bibi <bibi6666667@kakao.com>
2021-06-03 14:32:26 +0900

    refactor : UserController URL 리팩토링 #39

--- src/main/java/com/codesquad/airbnb/controller/UserController.java
+++ src/main/java/com/codesquad/airbnb/controller/UserController.java
@@ -3,13 +3,19 @@ package com.codesquad.airbnb.controller;
 import com.codesquad.airbnb.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
+
+    private static final String ENDPOINT = "https://accounts.google.com/o/oauth2/v2/auth";
+    private static final String CLIENT_ID = "";
+    private static final String REDIRECT_URI = "http://localhost:8080/oauth/google/callback";
+    private static final String RESPONSE_TYPE = "code";
+    private static final String SCOPE = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile";
 
     private UserService userService;
 
